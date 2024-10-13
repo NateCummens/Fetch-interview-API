acfc9946ac0d7ce2c62ac9a4bfe19024691d55d8
NateCummens <78138781+NateCummens@users.noreply.github.com>
2024-10-13 16:01:22 -0400

    added error statuses and handling

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ const PORT = 3000;
 app.use(express.json());
 app.use('/receipts', receipts);
 
+app.all("*",(req:Request, res:Response)=>{
+    res.status(404).send({error:"Page not found"})
+})
+
 app.listen(
     PORT,
     () => console.log(`Server is running on http://localhost:${PORT}`)
--- src/routes/receipts.ts
+++ src/routes/receipts.ts
@@ -8,46 +8,59 @@ const router: Router = Router();
 let datastore:ReceiptPoints = {};
 
 router.post("/process", (req:Request, res:Response) =>{
-    //get the json receipt in the body
-    const receipt = req.body
-    //process the data in the receipt
-   let points = receipt.retailer.replace(/\W/g, '').length;
-   if(receipt.total%1 === 0){
-    points += 50;
-   }
-   if(receipt.total%.25 === 0){
-    points += 25;
-   }
-   points += 5 * Math.floor(receipt.items.length/2);
-   receipt.items.forEach((item:any)=>{
-    if(item.shortDescription.trim().length %3 === 0){
-        points += Math.ceil(item.price * 0.2);
+    try {
+        //get the json receipt in the body
+        const receipt = req.body
+        //process the data in the receipt
+        let points = receipt.retailer.replace(/\W/g, '').length;
+        if(receipt.total%1 === 0){
+            points += 50;
+        }
+        if(receipt.total%.25 === 0){
+            points += 25;
+        }
+        points += 5 * Math.floor(receipt.items.length/2);
+        receipt.items.forEach((item:any)=>{
+            if(item.shortDescription.trim().length %3 === 0){
+                points += Math.ceil(item.price * 0.2);
+            }
+        })
+        if(receipt.purchaseDate.slice(-2)%2 === 1){
+            points += 6;
+        }
+        const time = receipt.purchaseTime.replace(":", '');
+        if(time > 1400 && time < 1600){
+            points += 10;
+        }
+        //generate a random ID for the amount of points
+        const newId:string = uuidv4();
+
+        //save data to datastore
+        datastore[newId] = points
+
+        console.log(datastore);
+
+        //return ID
+        res.send({id:newId})
+    } catch (error) {
+        res.sendStatus(400);
     }
-   })
-   if(receipt.purchaseDate.slice(-2)%2 === 1){
-    points += 6;
-   }
-   const time = receipt.purchaseTime.replace(":", '');
-   if(time > 1400 && time < 1600){
-    points += 10;
-   }
-   //generate a random ID for the amount of points
-   const newId:string = uuidv4();
-
-   //save data to datastore
-   datastore[newId] = points
-
-   console.log(datastore);
-
-   //return ID
-    res.send({id:newId})
 })
 
 router.get("/:id/points", (req:Request, res:Response) =>{
-    //get ID from path
-    const id = req.params.id;
-    //return points attached to ID
-    res.send({points:datastore[id]})
+    try {
+        //get ID from path
+        const id = req.params.id;
+        //return points attached to ID
+        if(datastore[id]){
+            res.send({points:datastore[id]})
+        } else{
+            res.sendStatus(404)
+        }
+    } catch (error) {
+        res.sendStatus(400);
+    }
+
 })
 
 export default router
