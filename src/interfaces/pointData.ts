3d8b2ee38f9856f3a1eb33d992709ef173c4ae03
NateCummens <78138781+NateCummens@users.noreply.github.com>
2024-11-04 16:22:46 -0500

    changes from interview
    I further adjusted the date validation to use regex

--- src/interfaces/pointData.ts
+++ src/interfaces/pointData.ts
@@ -1,3 +1,11 @@
 export interface ReceiptPoints{
     [key:string]:number
 }
+
+export interface User {
+    [key:string]:{
+        receipts: ReceiptPoints[]
+        amount:number
+    }
+
+}
--- src/routes/receipts.ts
+++ src/routes/receipts.ts
@@ -1,16 +1,27 @@
-import { Router, Request, Response } from "express";
-import { ReceiptPoints } from "../interfaces/pointData";
+import {Router, Request, Response, NextFunction } from "express";
+import { ReceiptPoints, User} from "../interfaces/pointData";
 
 const { v4: uuidv4 } = require('uuid');
 
 const router: Router = Router();
 
-let datastore:ReceiptPoints = {};
+// let datastore:ReceiptPoints = {};
 
-router.post("/process", (req:Request, res:Response) =>{
+let userData: User={}
+let userId = uuidv4();
+userData[userId] = {
+    receipts:[],
+    amount: 0
+}
+console.log(userData);
+router.post("/process", async (req:Request, res:Response): Promise<void> =>{
     try {
         //get the json receipt in the body
         const receipt = req.body
+        if(!/^((?:19|20)\d\d)[- \/.](0[1-9]|1[012])[- \/.](0[1-9]|[12][0-9]|3[01])$/.test(receipt.purchaseDate)){
+            res.sendStatus(400);
+            return;
+        }
         //process the data in the receipt
         let points = receipt.retailer.replace(/\W/g, '').length;
         if(receipt.total%1 === 0){
@@ -34,9 +45,30 @@ router.post("/process", (req:Request, res:Response) =>{
         }
         //generate a random ID for the amount of points
         const newId:string = uuidv4();
+        const receiptId:string = uuidv4();
 
         //save data to datastore
-        datastore[newId] = points
+        //1000, 500, 250
+        userData[userId].amount += 1;
+        switch(userData[userId].amount){
+            case(1):
+                points += 1000;
+                break;
+            case(2):
+                points += 500;
+                break;
+            case(3):
+                points += 250;
+                break;
+            default:
+                break;
+        }
+
+        let datastore:ReceiptPoints = {};
+        datastore[newId] = points;
+
+        userData[userId].receipts.push(datastore)
+        console.log(userData)
 
         //return ID
         res.send({id:newId})
@@ -45,16 +77,19 @@ router.post("/process", (req:Request, res:Response) =>{
     }
 })
 
-router.get("/:id/points", (req:Request, res:Response) =>{
+router.get("/:id/points", async (req:Request, res:Response) =>{
     try {
         //get ID from path
         const id = req.params.id;
         //return points attached to ID
-        if(datastore[id]){
-            res.send({points:datastore[id]})
-        } else{
-            res.sendStatus(404)
-        }
+        userData[userId].receipts.forEach((receipt)=>{
+            if(receipt[id]){
+                res.send({points:receipt[id]})
+            } else{
+                res.sendStatus(404)
+            }
+        })
+
     } catch (error) {
         res.sendStatus(400);
     }
